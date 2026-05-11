1f6b7f82c2cac7c49a92668b34e0a40a9fa828f3
chander3012 <chanderprakash3012@gmail.com>
2026-05-11 15:47:17 +0530

    deploy backend

--- frontend/src/pages/userdashboard/DashboardPage.jsx
+++ frontend/src/pages/userdashboard/DashboardPage.jsx
@@ -1,5 +1,6 @@
+
+
 import React, {
-  useEffect,
   useState,
 } from "react";
 
@@ -26,50 +27,15 @@ function DashboardPage() {
   const [activeFilter, setActiveFilter] =
     useState("today");
 
-  /* -----------------------------
-     MONETAG IN-PAGE PUSH
-  ----------------------------- */
-
-  useEffect(() => {
-    const monetag =
-      document.createElement(
-        "script"
-      );
-
-    monetag.dataset.zone =
-      "10992019";
-
-    monetag.src =
-      "https://nap5k.com/tag.min.js";
-
-    monetag.async = true;
-
-    document.body.appendChild(
-      monetag
-    );
-
-    return () => {
-      if (
-        document.body.contains(
-          monetag
-        )
-      ) {
-        document.body.removeChild(
-          monetag
-        );
-      }
-    };
-  }, []);
-
   return (
     <div
       className="
-        max-w-7xl
-        mx-auto
-        p-4
-        sm:p-6
-        lg:p-8
-        space-y-8
+        max-w-7xl 
+        mx-auto 
+        p-4 
+        sm:p-6 
+        lg:p-8 
+        space-y-8 
         lg:space-y-12
       "
       style={{
@@ -80,14 +46,14 @@ function DashboardPage() {
 
       <div
         className="
-          w-full
-          rounded-2xl
-          p-6
-          sm:p-8
-          grid
-          grid-cols-1
-          md:grid-cols-2
-          gap-8
+          w-full 
+          rounded-2xl 
+          p-6 
+          sm:p-8 
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-8 
           items-center
         "
         style={{
@@ -116,9 +82,9 @@ function DashboardPage() {
 
       <div
         className="
-          rounded-2xl
-          p-3
-          sm:p-6
+          rounded-2xl 
+          p-3 
+          sm:p-6 
           w-full
         "
         style={{
